67d6a966a616ee89e03f4f398024b9ec7be1781c
Indra Mahesa <indramahesa128@gmail.com>
2023-05-28 00:41:36 +0700

    fix: otp view model missing

---
+++ DinotisSampleProject/Feature/Auth/ViewModel/OtpViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  OtpViewModel.swift
+//  DinotisSampleProject
+//
+//  Created by Indra Mahesa on 26/05/23.
+//
+
+import SwiftUI
+import DinotisDesignSystem
+
+final class OtpViewModel: ObservableObject {
+    @Published var otp = ""
+}
